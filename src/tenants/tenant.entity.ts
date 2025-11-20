a2474a8f0f97f03bf52b30967d2deb322d4d7467
Sameer-Abdul <abdulsameer8787@gmail.com>
2025-11-20 19:43:31 +0530

    update 3.9

--- src/tenants/tenant.entity.ts
+++ src/tenants/tenant.entity.ts
@@ -17,10 +17,10 @@ export class Tenant {
   @Column({ nullable: true })
   address: string;
 
-  @Column({ nullable: true })
+  @Column({ name: 'image_url_left', nullable: true })
   image_left: string;
 
-  @Column({ nullable: true })
+  @Column({ name: 'image_url_right', nullable: true })
   image_right: string;
 
   @Column({ default: 'AUTO' })
